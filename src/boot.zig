5383c188c18e6455a702b966c1fd38394f51a3df
ssstoyama <ssstoyama.develop@gmail.com>
2023-04-05 14:07:23 +0000

    add initialization  bootloader

--- src/boot.zig
+++ src/boot.zig
@@ -2,16 +2,25 @@ const std = @import("std");
 const uefi = std.os.uefi;
 
 var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
+var bs: *uefi.tables.BootServices = undefined;
+var fs: *uefi.protocols.SimpleFileSystemProtocol = undefined;
 
 pub fn main() uefi.Status {
-    con_out = uefi.system_table.con_out orelse return .Unsupported;
-
     var status: uefi.Status = undefined;
 
+    con_out = uefi.system_table.con_out orelse return .Unsupported;
+    defer {
+        printf("boot error: status={d}\r\n", .{status});
+        while (true) asm volatile ("hlt");
+    }
     status = con_out.clearScreen();
     if (status != .Success) return status;
 
-    printf("Hello, {s}!\r\n", .{"Loader"});
+    bs = uefi.system_table.boot_services orelse return .Unsupported;
+    status = bs.locateProtocol(&uefi.protocols.SimpleFileSystemProtocol.guid, null, @ptrCast(*?*anyopaque, &fs));
+    if (status != .Success) return status;
+
+    printf("initialized boot loader\r\n", .{});
 
     while (true) {}
 
