4e1d136b5d7ec9d57c6f811106edbe0e693fb3f9
ssstoyama <ssstoyama.develop@gmail.com>
2023-04-08 12:09:15 +0000

    improve comments and error handling

--- src/boot.zig
+++ src/boot.zig
@@ -2,39 +2,60 @@ const std = @import("std");
 const uefi = std.os.uefi;
 const elf = std.elf;
 
-var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 var bs: *uefi.tables.BootServices = undefined;
+var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 var fs: *uefi.protocols.SimpleFileSystemProtocol = undefined;
+var gop: *uefi.protocols.GraphicsOutputProtocol = undefined;
 
 pub fn main() uefi.Status {
     var status: uefi.Status = undefined;
 
-    // 1. ブートローダーの初期化処理
+    // ----------------------
+    // ブートローダーの初期化処理
+    // ----------------------
 
     // SimpleTextOutputProtocol 取得
     con_out = uefi.system_table.con_out orelse return .Unsupported;
-    defer {
-        printf("boot error: status={d}\r\n", .{status});
-        while (true) asm volatile ("hlt");
-    }
+    // 画面クリア
     status = con_out.clearScreen();
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to clear screen: {d}\r\n", .{status});
+        return status;
+    }
 
     // BootServices 取得
-    bs = uefi.system_table.boot_services orelse return .Unsupported;
+    bs = uefi.system_table.boot_services orelse {
+        printf("unsupported boot services\r\n", .{});
+        return .Unsupported;
+    };
 
     // SimpleFileSystemProtocol 取得
     status = bs.locateProtocol(&uefi.protocols.SimpleFileSystemProtocol.guid, null, @ptrCast(*?*anyopaque, &fs));
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to locate file system protocol: {d}\r\n", .{status});
+        return status;
+    }
+
+    // GraphicsOutputProtocol 取得
+    status = bs.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*anyopaque, &gop));
+    if (status != .Success) {
+        printf("failed to locate graphics output protocol: {d}\r\n", .{status});
+        return status;
+    }
 
     printf("initialized boot loader\r\n", .{});
 
-    // 2. カーネルのヘッダを読み込む
+    // ----------------------
+    // カーネルヘッダ取得
+    // ----------------------
 
     // ルートディレクトリを開く
     var root_dir: *uefi.protocols.FileProtocol = undefined;
     status = fs.openVolume(&root_dir);
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to open root directory: {d}\r\n", .{status});
+        return status;
+    }
     printf("opened root directory\r\n", .{});
 
     // カーネルファイルを開く
@@ -45,31 +66,48 @@ pub fn main() uefi.Status {
         uefi.protocols.FileProtocol.efi_file_mode_read,
         uefi.protocols.FileProtocol.efi_file_read_only,
     );
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to open kernel file: {d}\r\n", .{status});
+        return status;
+    }
     printf("opened kernel file\r\n", .{});
 
+    // ヘッダ読み込み用のバッファ
     var header_buffer: [*]align(8) u8 = undefined;
     var header_size: usize = @sizeOf(elf.Elf64_Ehdr);
-    // ヘッダ読み込み用のメモリを用意する
+    // ヘッダ読み込み用のメモリを確保する
     status = bs.allocatePool(uefi.tables.MemoryType.LoaderData, header_size, &header_buffer);
-    if (status != .Success) return status;
-    // kernel.elf からヘッダを読み込む
+    if (status != .Success) {
+        printf("failed to allocate memory for kernel header: {d}\r\n", .{status});
+        return status;
+    }
+    // kernel.elf の先頭からヘッダのみ読み込む
     status = kernel_file.read(&header_size, header_buffer);
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to read kernel header: {d}\r\n", .{status});
+        return status;
+    }
     // header_buffer を Header 構造体にパースする
     const header = elf.Header.parse(header_buffer[0..@sizeOf(elf.Elf64_Ehdr)]) catch |err| {
         printf("failed to parse kernel header: {}\r\n", .{err});
         return .LoadError;
     };
-    printf("read kernel header: entry_point=0x{x}\r\n", .{header.entry});
+    printf("read kernel header\r\n", .{});
 
-    // 3. カーネルのロードに必要なメモリを確保する
+    // ----------------------
+    // カーネルのロード
+    // ----------------------
 
     // カーネルのロードに必要なメモリを確保するためにページ数(1ページ=4KiB)を計算する
     var kernel_first_addr: elf.Elf64_Addr align(4096) = std.math.maxInt(elf.Elf64_Addr);
     var kernel_last_addr: elf.Elf64_Addr = 0;
     var iter = header.program_header_iterator(kernel_file);
-    while (try iter.next()) |phdr| {
+    while (true) {
+        // プログラムヘッダを1つ読み込む
+        const phdr = iter.next() catch |err| {
+            printf("failed to iterate program headers: {}\r\n", .{err});
+            return .LoadError;
+        } orelse break;
         // プログラムヘッダタイプ LOAD 以外はスキップ
         if (phdr.p_type != elf.PT_LOAD) continue;
         if (phdr.p_vaddr < kernel_first_addr) {
@@ -79,47 +117,55 @@ pub fn main() uefi.Status {
             kernel_last_addr = phdr.p_vaddr + phdr.p_memsz;
         }
     }
-    // カーネルが収まるようにページ数(=メモリのサイズ)を計算する
     const pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000; // 0x1000=4096
     printf("kernel first addr: 0x{x}, kernel last addr: 0x{x}, pages=0x{x}\r\n", .{ kernel_first_addr, kernel_last_addr, pages });
 
     // kernel_first_addr から pages ページ分のメモリを確保する
     status = bs.allocatePages(.AllocateAddress, .LoaderData, pages, @ptrCast(*[*]align(4096) u8, &kernel_first_addr));
-    if (status != .Success) return status;
+    if (status != .Success) {
+        printf("failed to allocate pages for kernel: {d}\r\n", .{status});
+        return status;
+    }
     printf("allocated pages for kernel\r\n", .{});
 
     iter = header.program_header_iterator(kernel_file);
-    while (try iter.next()) |phdr| {
+    while (true) {
+        // プログラムヘッダを1つ読み込む
+        const phdr = iter.next() catch |err| {
+            printf("failed to iterate program headers: {}\r\n", .{err});
+            return .LoadError;
+        } orelse break;
+        // プログラムヘッダタイプ LOAD 以外はスキップ
         if (phdr.p_type != elf.PT_LOAD) continue;
 
-        // ファイルの読み込み位置をセットする
+        // ファイルの読み込み位置をあわせる
+        // phdr.p_offset はカーネルファイルの先頭からのオフセット
         status = kernel_file.setPosition(phdr.p_offset);
-        if (status != .Success) return status;
+        if (status != .Success) {
+            printf("failed to set file position: {d}\r\n", .{status});
+            return status;
+        }
 
-        // カーネルセグメント読み込み先のメモリ
+        // セグメント読み込み先のポインタ
         var segment: [*]u8 = @intToPtr([*]u8, phdr.p_vaddr);
-        // カーネルセグメントのサイズ
+        // セグメントのサイズ
         var mem_size: usize = phdr.p_memsz;
-        // メモリにカーネルのセグメントを読み込む
+        // メモリにセグメントを読み込む
         status = kernel_file.read(&mem_size, segment);
-        if (status != .Success) return status;
+        if (status != .Success) {
+            printf("failed to load segment: {d}\r\n", .{status});
+            return status;
+        }
         printf(
             "load segment: addr=0x{x}, offset=0x{x}, mem_size=0x{x}\r\n",
             .{ phdr.p_vaddr, phdr.p_offset, phdr.p_memsz },
         );
     }
 
-    // 不要になったメモリ、ファイルの後始末
-    status = bs.freePool(header_buffer);
-    if (status != .Success) return status;
-    status = kernel_file.close();
-    if (status != .Success) return status;
-    status = root_dir.close();
-    if (status != .Success) return status;
+    // ----------------------
+    // カーネルに渡す情報(BootInfo)を用意する
+    // ----------------------
 
-    var gop: *uefi.protocols.GraphicsOutputProtocol = undefined;
-    status = bs.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*anyopaque, &gop));
-    if (status != .Success) return status;
     const frame_buffer_config = FrameBufferConfig{
         .frame_buffer = @intToPtr([*]u8, gop.mode.frame_buffer_base),
         .pixels_per_scan_line = gop.mode.info.pixels_per_scan_line,
@@ -131,12 +177,49 @@ pub fn main() uefi.Status {
             else => unreachable,
         },
     };
-
-    // 4. カーネルを実行する
     const boot_info = BootInfo{
         .frame_buffer_config = &frame_buffer_config,
     };
+    printf("kernel entry point: 0x{x}\r\n", .{header.entry});
     printf("boot info pointer: {*}\r\n", .{&boot_info});
+
+    // ----------------------
+    // ブートサービス終了処理
+    // ----------------------
+
+    // 不要になったメモリ、ファイルの後始末
+    status = bs.freePool(header_buffer);
+    if (status != .Success) {
+        printf("failed to free memory for kernel header: {d}\r\n", .{status});
+        return status;
+    }
+    status = kernel_file.close();
+    if (status != .Success) {
+        printf("failed to close kernel file: {d}\r\n", .{status});
+        return status;
+    }
+    status = root_dir.close();
+    if (status != .Success) {
+        printf("failed to close root directory: {d}\r\n", .{status});
+        return status;
+    }
+
+    // map_key を取得してブートローダーを終了する
+    var map_size: usize = 0;
+    var descriptors: [*]uefi.tables.MemoryDescriptor = undefined;
+    var map_key: usize = 0;
+    var descriptor_size: usize = 0;
+    var descriptor_version: u32 = 0;
+    _ = bs.getMemoryMap(&map_size, descriptors, &map_key, &descriptor_size, &descriptor_version);
+    status = bs.exitBootServices(uefi.handle, map_key);
+    if (status != .Success) {
+        printf("failed to exit boot services: {d}\r\n", .{status});
+        return status;
+    }
+
+    // ----------------------
+    // カーネル呼び出し
+    // ----------------------
     startKernel(header.entry, &boot_info);
 
     return .LoadError;
