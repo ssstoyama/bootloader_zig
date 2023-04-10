762fe61b16b14b1b41bc8321e2e3628969a238f4
ssstoyama <ssstoyama.develop@gmail.com>
2023-04-10 13:19:36 +0000

    add optimize option to build.zig

--- build.zig
+++ build.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const is_debug = b.option(bool, "debug", "debugging in qemu") orelse false;
+
+    var optimize: std.builtin.Mode = .ReleaseSafe;
+    if (is_debug) {
+        optimize = .Debug;
+    }
+
     const loader = b.addExecutable(.{
         .name = "bootx64",
         .root_source_file = .{ .path = "src/boot.zig" },
@@ -8,6 +15,7 @@ pub fn build(b: *std.Build) !void {
             .cpu_arch = .x86_64,
             .os_tag = .uefi,
         },
+        .optimize = optimize,
         .linkage = .static,
     });
     loader.setOutputDir("fs/efi/boot");
@@ -21,6 +29,7 @@ pub fn build(b: *std.Build) !void {
             .os_tag = .freestanding,
             .ofmt = .elf,
         },
+        .optimize = optimize,
         .linkage = .static,
     });
     kernel.image_base = 0x100000;
@@ -30,7 +39,7 @@ pub fn build(b: *std.Build) !void {
     kernel.setOutputDir("fs");
     kernel.install();
 
-    const qemu = qemuCommand(b);
+    const qemu = qemuCommand(b, is_debug);
     qemu.step.dependOn(&loader.step);
     qemu.step.dependOn(&kernel.step);
 
@@ -38,9 +47,7 @@ pub fn build(b: *std.Build) !void {
     run_qemu.dependOn(&qemu.step);
 }
 
-fn qemuCommand(b: *std.Build) *std.Build.RunStep {
-    const is_debug = b.option(bool, "debug", "debugging in qemu") orelse false;
-
+fn qemuCommand(b: *std.Build, is_debug: bool) *std.Build.RunStep {
     const args = [_][]const u8{
         "qemu-system-x86_64",
         "-m",
