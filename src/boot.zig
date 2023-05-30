18a0b837764a0868d7779c6e43f225883f0a6dcf
ssstoyama <ssstoyama.develop@gmail.com>
2023-05-30 13:40:38 +0000

    improve call kernel_main

--- src/boot.zig
+++ src/boot.zig
@@ -228,7 +228,8 @@ pub fn main() uefi.Status {
     // ----------------------
     // カーネル呼び出し
     // ----------------------
-    startKernel(header.entry, &boot_info);
+    const kernel_main = @intToPtr(*fn (*const BootInfo) callconv(.SysV) void, header.entry);
+    kernel_main(&boot_info);
 
     return .LoadError;
 }
@@ -249,17 +250,6 @@ pub const PixelFormat = enum(u8) {
     PixelRGBResv8BitPerColor = 1,
     PixelBGRResv8BitPerColor = 2,
 };
-
-fn startKernel(entry_point: u64, boot_info: *const BootInfo) void {
-    // RDI レジスタ(第1引数)に BootInfo 構造体へのポインタを渡す
-    // RAX レジスタに関数のアドレスを渡して実行する
-    asm volatile (
-        \\ callq *%rax
-        :
-        : [entry_point] "{rax}" (entry_point),
-          [boot_info] "{rdi}" (boot_info),
-    );
-}
 
 fn printf(comptime format: []const u8, args: anytype) void {
     var buf: [1024]u8 = undefined;
